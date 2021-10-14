4c987b1ce79c6ac30b1ecbb81f8dcf7f0ddfe339
Pipo1999 <ivan.benissa@gmail.com>
2021-10-14 14:48:45 +0200

    3

---
+++ act301/301cards-view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>301Cards</title>
+</head>
+<body>
+
+<?php
+require "301cards.php";
+
+foreach ($cards as $carta){
+    echo "<p>".$carta -> getSuit()." - ".$carta -> getSymbol()." - ".$carta -> getValue()."</p>";
+}
+
+?>
+
+</body>
+</html>
