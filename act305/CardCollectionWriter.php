d9697e8550fc1317ef226224f368c28de69371bf
Pipo1999 <72856588+Pipo1999@users.noreply.github.com>
2021-10-28 08:43:57 +0200

    Delete act305 directory

--- act305/CardCollectionWriter.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CardCollectionWriter {
-    public function write(CardCollection $cardDeck) {
-        foreach ($cardDeck->getCards() as $card) {
-            echo sprintf("<div>%s - %s </div>", $card->getSymbol(), $card->getSuit());
-        }
-    }
-}
-?>
--- act305/Hand.php
+++
@@ -1,6 +0,0 @@
-<?php
-class hand extends CardCollection {
-    function play(): Card {
-        return array_shift($this->cards);
-    }
-}
